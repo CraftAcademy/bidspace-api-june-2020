7b823fe0e9a3bd129a6a65f36a3c264815e2fa77
Sima <sima.boreiri@gmail.com>
2020-08-31 15:40:05 +0200

    tests the sad path, adds the unit test for database and factory bot

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have a valid Factory' do
+    expect(create(:listing)).to be_valid
+  end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :category }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :scene }
+    it { is_expected.to have_db_column :height }
+    it { is_expected.to have_db_column :address }
+    it { is_expected.to have_db_column :price }
+  end
 end
--- spec/requests/api/v1/listings/index_spec.rb
+++ spec/requests/api/v1/listings/index_spec.rb
@@ -13,6 +13,15 @@ RSpec.describe 'GET /api/v1/listings', type: :request do
     it 'should return listings' do
     expect(response_json["listings"].count).to eq 3
     end
-
   end 
+
+  describe 'no listing has been added' do
+    before do
+      get '/api/v1/listings'
+    end
+
+    it 'should have no articles on page' do
+      expect(response_json["listings"]).to eq []
+    end
+  end
 end
