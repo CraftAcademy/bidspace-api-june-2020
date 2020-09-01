987c2e7bff41ce73f2083aa87a760925efeb04e1
odeane_croney <odeane_croney@yahoo.com>
2020-09-01 15:22:47 +0200

    formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-	private
-	
+  private
+
   def render_error_message(errors)
     error_message = if !errors.class.method_defined?(:full_messages)
-    	errors.message
-  	else
-    	errors.full_messages.to_sentence
- 	  end
+                      errors.message
+                    else
+                      errors.full_messages.to_sentence
+     end
     render json: { message: error_message }, status: 422
   end
 end
