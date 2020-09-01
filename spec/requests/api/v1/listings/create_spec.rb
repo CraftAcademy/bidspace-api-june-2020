72ea2fdfd11494a41215c2543de4fc5f1850ae3a
Sima <sima.boreiri@gmail.com>
2020-09-01 09:53:53 +0200

    writes the test for creating listing by landlord

---
+++ spec/requests/api/v1/listings/create_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe "POST /api/v1/listings", type: :request do 
+
+  let!(:landlord) { create(:user, role: 'landlord')}
+  let!(:landlord_credentials) { landlord.create_new_auth_token }
+  let!(:landlord_headers) { { HTTP_ACCEPT: 'application/json'}}.merge!(landlord_credentials)
+
+  describe 'successfully with valid params and headers' do 
+    before do 
+      post '/api/v1/listings',
+      params: {
+        listing: {
+          category: 'car park',
+          lead: 'Too good to park',
+          scene: 'outdoor',
+          address:'Småbrukets backe 30 14158 Huddinge',
+          description: "This is an outdoor parking but in safe area",
+          price: 150
+        }
+      }, headers: landlord_headers
+    end
+
+    it 'is expected to return 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'is expected to return success message' do
+      expect(response_json['message']).to eq 'The listing has been created successfully!'
+    end
+
+    it 'is expected to create listing' do
+      listing = Listing.last
+      expect(listing.category).to eq 'car park'
+    end
+
+    it ' listing is expected to be associated with landlord' do
+      expect(landlord.listings.first.lead).to eq 'Too good to park'
+    end
+
+    it 'listing is expected to have longitude and latitude' do
+      listing = Listing.last
+      expect(listing.longitude).to eq 17.953150 
+    end
+  end
+end
