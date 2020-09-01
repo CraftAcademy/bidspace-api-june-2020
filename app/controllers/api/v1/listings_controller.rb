3aea05e2e10faca8b162f5520e30fcd815c4f65f
odeane_croney <odeane_croney@yahoo.com>
2020-09-01 19:57:40 +0200

    adding image decode service and attaching images to listing

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -18,7 +18,8 @@ class Api::V1::ListingsController < ApplicationController
 
   def create
     listing = current_user.listings.create(listing_params)
-    if listing.persisted?
+    if listing.persisted? && attach_image(listing)
+      binding.pry
       render json: { message: 'The listing has been created successfully!' }
     else
       render_error_message(listing.errors)
@@ -28,6 +29,13 @@ class Api::V1::ListingsController < ApplicationController
   end
 
   private
+  
+  def attach_image(listing)
+    params_image = params[:listing][:image]
+    if params_image.present?
+      DecodeService.attach_image(params_image, listing.image)
+    end
+  end
 
   def listing_params
     params.require(:listing).permit(:category, :lead, :scene, :description, :price, :address)
