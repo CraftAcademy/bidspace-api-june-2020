6acceb79253d20d8f01fb22a9de6c530350efce0
odeane_croney <odeane_croney@yahoo.com>
2020-09-03 20:33:28 +0200

    formatting

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -20,27 +20,26 @@ class Api::V1::ListingsController < ApplicationController
     if current_user.role == 'subscriber'
       listing_handler
     else
-    listing = current_user.listings.create(listing_params)
-    if current_user.listings.size < 3
-     listing_handler
-    else
-      render json: { message: "Subscribe to create more listing."}, status:422
-    end
+      listing = current_user.listings.create(listing_params)
+      if current_user.listings.size < 3
+        listing_handler
+      else
+        render json: { message: 'Subscribe to create more listing.' }, status: 422
+      end
   end
   rescue StandardError => e
     render json: { message: e }, status: 422
   end
 
   private
-  
+
   def attach_image(listing)
-    
     params_images = params[:listing][:images]
     if params_images.present?
-      params_images.each do |image| 
-        DecodeService.attach_image(image, listing.image) 
+      params_images.each do |image|
+        DecodeService.attach_image(image, listing.image)
       end
-      
+
     end
   end
 
@@ -49,15 +48,14 @@ class Api::V1::ListingsController < ApplicationController
   end
 
   def listing_handler
-     listing = current_user.listings.create(listing_params)
-      if listing.persisted? && attach_image(listing)
-        render json: { message: 'The listing has been created successfully!' }
-      elsif !attach_image(listing)
-        listing.destroy
-        render json: { message: "The image can't be blank"}, status:422
-      else 
-        render_error_message(listing.errors)
-      end
+    listing = current_user.listings.create(listing_params)
+    if listing.persisted? && attach_image(listing)
+      render json: { message: 'The listing has been created successfully!' }
+    elsif !attach_image(listing)
+      listing.destroy
+      render json: { message: "The image can't be blank" }, status: 422
+    else
+      render_error_message(listing.errors)
+    end
   end
-
 end
--- spec/requests/api/v1/listings/listing_limitation_spec.rb
+++ spec/requests/api/v1/listings/listing_limitation_spec.rb
@@ -1,28 +1,30 @@
-RSpec.describe 'POST /api/v1/listings', type: :request do
-let!(:landlord) {create(:user, role: 'registered')}
-let!(:listing) {5.times {create(:listing, landlord: landlord)}}
-let!(:landlord_credentials) { landlord.create_new_auth_token }
-let!(:landlord_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(landlord_credentials) }
+# frozen_string_literal: true
 
-let(:images) {
+RSpec.describe 'POST /api/v1/listings', type: :request do
+  let!(:landlord) { create(:user, role: 'registered') }
+  let!(:listing) { 5.times { create(:listing, landlord: landlord) } }
+  let!(:landlord_credentials) { landlord.create_new_auth_token }
+  let!(:landlord_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(landlord_credentials) }
+
+  let(:images) do
     [
-      { 
+      {
         type: 'application/json',
         encoder: 'iphone_picture',
         data: 'AEwughvcvjdkshdhdcdcgWEgvcdhhd',
         extension: 'jpg'
       },
-      { 
+      {
         type: 'application/json',
         encoder: 'parking_picture',
         data: 'DfhgxhsxsvttfusxSDDDJhcdcdc',
         extension: 'jpg'
       }
     ]
-  }
+  end
 
-describe 'Reg user can only successfully create two listing' do
-  before do
+  describe 'Reg user can only successfully create two listing' do
+    before do
       post '/api/v1/listings',
            params: {
              listing: {
@@ -33,50 +35,50 @@ describe 'Reg user can only successfully create two listing' do
                description: 'This is an outdoor parking but in safe area',
                price: 150,
                images: images
-             },
+             }
            }, headers: landlord_headers
-  end
+    end
 
-  it 'is expected to return 422 response status' do
+    it 'is expected to return 422 response status' do
       expect(response).to have_http_status 422
+    end
+
+    it 'is expect to return error message when creating third listing' do
+      expect(response_json['message']).to eq 'Subscribe to create more listing.'
+    end
   end
 
-  it 'is expect to return error message when creating third listing' do
-    expect(response_json['message']).to eq 'Subscribe to create more listing.'
-  end
- end
+  describe 'subscriber can successfully create more then two listings' do
+    let(:landlord) { create(:user, role: 'subscriber') }
+    let(:landlord_credentials) { landlord.create_new_auth_token }
+    let(:landlord_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(landlord_credentials) }
+    let!(:listing) { 5.times { create(:listing, landlord: landlord) } }
 
- describe 'subscriber can successfully create more then two listings' do
-  let(:landlord) { create(:user, role: "subscriber") }
-  let(:landlord_credentials) { landlord.create_new_auth_token }
-  let(:landlord_headers) { { HTTP_ACCEPT: "application/json" }.merge!(landlord_credentials) }
-  let!(:listing) {5.times {create(:listing, landlord: landlord)}}
+    before do
+      post '/api/v1/listings',
+           params: {
+             listing: {
+               category: 'car park',
+               lead: 'Too good to park',
+               scene: 'outdoor',
+               address: 'Småbrukets backe 30 14158 Huddinge',
+               description: 'This is an outdoor parking but in safe area',
+               price: 150,
+               images: images
+             }
+           }, headers: landlord_headers
+    end
 
-  before do
-    post '/api/v1/listings',
-         params: {
-           listing: {
-             category: 'car park',
-             lead: 'Too good to park',
-             scene: 'outdoor',
-             address: 'Småbrukets backe 30 14158 Huddinge',
-             description: 'This is an outdoor parking but in safe area',
-             price: 150,
-             images: images
-           },
-         }, headers: landlord_headers
-  end
+    it 'is expected to return 200 response status' do
+      expect(response).to have_http_status 200
+    end
 
-  it 'is expected to return 200 response status' do
-    expect(response).to have_http_status 200
-  end
+    it 'is expected to return success message' do
+      expect(response_json['message']).to eq 'The listing has been created successfully!'
+    end
 
-  it 'is expected to return success message' do
-    expect(response_json['message']).to eq 'The listing has been created successfully!'
+    it 'is expected to have six listings created' do
+      expect(Listing.all.count).to eq 6
+    end
   end
-
-  it 'is expected to have six listings created' do
-    expect(Listing.all.count).to eq 6
-  end
- end
 end
