8c3734d8340dd6e51ab33a4c36b3558e27c5a6f5
viktor_sommar <vsommarbaillot@gmail.com>
2020-09-07 21:13:40 +0200

    Adding a sad path to the test

--- spec/requests/api/v1/account/account_index_spec.rb
+++ spec/requests/api/v1/account/account_index_spec.rb
@@ -1,7 +1,11 @@
 RSpec.describe "GET '/api/v1/account/listings" do
   let!(:landlord) { create(:user)}
+  let!(:subscriber) { create(:user)}
 
-  let!(:listing) { 2.times { create(:listing, landlord_id: landlord.id) } }
+  let!(:listing_1) { create(:listing, lead: 'The first lead', scene: 'indoor', landlord_id: landlord.id) }
+  let!(:listing_2) { create(:listing, lead: 'The second lead', scene: 'indoor', landlord_id: subscriber.id) }
+  let!(:listing_3) { create(:listing, lead: 'The third lead', scene: 'indoor', landlord_id: subscriber.id) }
+  let!(:listing_4) { create(:listing, lead: 'The fourth lead', scene: 'outdoor', landlord_id: landlord.id) }
 
   describe 'landlord successfully get published listings' do
     let(:landlord_credentials) { landlord.create_new_auth_token }
@@ -21,11 +25,26 @@ RSpec.describe "GET '/api/v1/account/listings" do
     end
 
     it 'should return landlord listings with lead' do
-      expect(response_json["listings"].first["lead"]).to eq 'vacant'
+      expect(response_json["listings"].first["lead"]).to eq 'The first lead'
     end
 
     it 'should return landlord listings with lead' do
-      expect(response_json["listings"].first["scene"]).to eq 'indoor'
+      expect(response_json["listings"].last["scene"]).to eq 'outdoor'
+    end
+  end
+
+
+  describe 'non-registered users unsuccessfully gets listings' do
+    before do
+      get '/api/v1/account/listings'
+    end
+
+    it "should return a 401 status" do 
+      expect(response).to have_http_status 401
+    end
+
+    it "is expected to return error message" do 
+      expect(response_json['errors'].first).to eq 'You need to sign in or sign up before continuing.'
     end
   end
 end
