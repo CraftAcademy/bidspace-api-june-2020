1e49000146a93c1cf88522a0565c4ee9f541bfd4
Alex_Bellamy <alexlbellamy@gmail.com>
2020-09-08 14:12:16 +0200

    writing initial tests user can accept or reject bid

---
+++ spec/requests/api/v1/account/accept_or_reject_bid_spec.rb
@@ -0,0 +1,57 @@
+RSpec.describe "PUT /account/listings", type: :request do
+  let!(:landlord) { create(:user)}
+  let(:landlord_credentials) { landlord.create_new_auth_token }
+  let(:landlord_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(landlord_credentials) }
+
+  let!(:subscriber) { create(:user)}
+
+  let!(:listing) do
+    create(
+      :listing,
+      :with_images,
+      category: 'Parking Spot',
+      lead: 'Big Space for big car',
+      scene: 'outdoor', address: 'Vasagatan 1, 40530 Göteborg',
+      description: 'Close to a kebab store',
+      price: 100,
+      landlord_id: landlord.id
+    )
+  end
+  let!(:bids) { 5.times { create(:bidding, listing_id: listing.id, user_id: subscriber.id)}}
+
+  describe "successfully accepted" do
+    before do
+      put "/api/v1/account/listings/#{listing.id}", 
+      params: { 
+          bidding: { accepted: true } 
+      },
+      headers: landlord_headers 
+    end
+
+    it "should return a 200 status" do
+      expect(response).to have_http_status 200
+    end
+
+    it "should return success message" do
+      expect(response_json['message']).to eq "You have accepted this bid!"
+    end
+  end
+
+  describe "successfully rejected" do
+    before do
+      put "/api/v1/account/listings/#{listing.id}", 
+      params: { 
+          bidding: { accepted: false } 
+      },
+      headers: landlord_headers 
+    end
+
+    it "should return a 200 status" do
+      expect(response).to have_http_status 200
+    end
+
+    it "should return success message" do
+      expect(response_json['message']).to eq "You have rejected this bid!"
+    end
+  end
+end
